df1fff94b071b7dc77ff57462161b4aad900e574
Tanner Plauché <tannerplauche@Tanners-MacBook-Pro.local>
2020-11-11 17:35:29 -0600

    basic crud

--- index.js
+++ index.js
@@ -11,10 +11,59 @@ mongoose.connect('mongodb://admin:admin1@ds013951.mlab.com:13951/todos',
         useUnifiedTopology: true, retryWrites: false
     }, () => {
         console.log('Connected to MongoDb!');
+    });
+
+app.use(bodyParser.json());
+
+app.get('/:username/todos', (req, res) => {
+    Todo.find({ user: req.params.username }, (err, todos) => {
+        if (err) {
+            console.log('err: ', err);
+            res.send({ error: `There was an error fetching your todos: ${err}` });
+        } else {
+            res.send(todos);
+        }
+    });
+})
+
+app.post('/:username/todos', (req, res) => {
+    let username = req.params.username;
+    let todo = {
+        ...req.body,
+        user: username
+    }
+    let newTodo = new Todo(todo);
+    newTodo.save().then((savedTodo) => {
+        res.send(savedTodo);
+    }).catch(err => {
+        res.send({ error: `There was an error fetching your todos: ${err}` });
     })
+});
 
+app.put('/:username/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let username = req.params.username;
+    let todo = req.body;
 
-app.use((req, res) => res.send('Hello world!'));
+    Todo.findOneAndUpdate({ _id: mongoose.Types.ObjectId(id) }, todo, { new: true }, (err, todo) => {
+        if (err) {
+            console.log('err: ', err);
+            res.send({ error: `There was an error updating your todos: ${JSON.stringify(err)}` });
+        } else {
+            res.send(todo);
+        }
+    });
+});
+
+app.delete('/:username/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let username = req.params.username;
+
+    Todo.deleteOne({ _id: mongoose.Types.ObjectId(id) }).then((value) => {
+        res.send(`${id} deleted`);
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
