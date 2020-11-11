2fd5abf9a58b2d795b76a9785bee2cc2ea16b156
Tanner Plauché <tannerplauche@Tanners-MacBook-Pro.local>
2020-11-11 17:07:43 -0600

    change port

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ mongoose.connect('mongodb://admin:admin1@ds013951.mlab.com:13951/todos',
 
 app.use((req, res) => res.send('Hello world!'));
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
+app.listen(8888, () => {
+    console.log('Listening on port 8888');
 })
