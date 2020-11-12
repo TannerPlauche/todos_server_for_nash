fd58d5d3b8e5680d052009af22761a3bdbda072f
Tanner Plauché <tannerplauche@Tanners-MacBook-Pro.local>
2020-11-11 19:56:11 -0600

    delete by user

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.delete('/:username/todos', (req, res) => {
     let username = req.params.username;
 
     Todo.deleteOne({ user: username }).then((value) => {
-        res.send(`${id} deleted`);
+        res.send(`${username}'s items deleted`);
     });
 });
 
